dbad25f878b0e75be638be54a4fdb64884af8837
grecio <g_recio@ugr.es>
2016-03-30 15:39:26 +0200

    Mi app Django Girls, primer commit

---
+++ programas/p1.py
@@ -0,0 +1,15 @@
+def hi():
+        print('Hi there!')
+        print('How are you?')
+
+print("Hello, Django girls")
+asignatura={'nombre':'MMI','creditos':6,'curso':2,'grado':'fisica'}
+asignatura['cuatrimestre']=1
+print(asignatura)
+if 3>2:
+	print('It works!')
+
+hi()
+
+
+
